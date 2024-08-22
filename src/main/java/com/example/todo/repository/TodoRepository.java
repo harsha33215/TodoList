5f809d05381f99079fec03a1a8ae5df4a1a36ee1
harshavardhan penumala <penumalaharshavardhan@gmail.com>
2024-08-22 18:38:29 +0530

    Update TodoRepository.java

--- src/main/java/com/example/todo/repository/TodoRepository.java
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -1,5 +1,3 @@
-// Write your code here
-
 package com.example.todo.repository;
 
 import java.util.*;
@@ -7,13 +5,15 @@ import java.util.*;
 import com.example.todo.model.Todo;
 
 public interface TodoRepository {
-    ArrayList<Todo> getAllTodos();
 
-    Todo getTodoById(int id);
+    ArrayList<Todo> getTodos();
+
+    Todo getTodoById(int todoId);
 
     Todo addTodo(Todo todo);
 
-    Todo updateTodo(int id, Todo todo);
+    Todo updateTodo(int todoId, Todo todo);
 
-    void deleteTodo(int id);
+    void deleteTodo(int todoId);
 }
+
